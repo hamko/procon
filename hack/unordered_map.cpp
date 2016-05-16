bed786c323cb9640fddd0a5c57a1dc21932003b0
hamko <wakataberyo@gmail.com>
2016-05-16 21:03:15 +0200

    added unordered_map hack

--- hack/unordered_map.cpp
+++ hack/unordered_map.cpp
@@ -1,47 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
-#define rep(i,n) for(int i = 0; i < n; i++)
-
-int main(void) {
-    cin.tie(0); ios::sync_with_stdio(false);
-    
-    ll n = 46794; // 1e9max
-//    ll n = 100000; 
-
-    unordered_map<ll, ll> a;
-//    a.reserve(n);
-
-#if 0
-    int j = 1;
-    ll pbcount = a.bucket_count();
-    rep(i, n) {
-        ll bcount = a.bucket_count();
-        if (bcount != pbcount) { pbcount = bcount; j = 1; /*cout << i << " " << bcount << endl;*/}
-        ll hack_index = bcount*j;
-        a[hack_index] = 1; // SUPER SLOW
-//        a[rand()%100] = 1; // this is fast
-        cout << i << " " << hack_index << endl;
-        j++;
+int main(){
+    unordered_map<long long, long long> s;
+    // super slow over 60000
+    for(int i = 0; i < 100000; ++i){
+        s[i*126271LL] = i;
+        cout << i << endl;
     }
-
-    cout << "end" << endl;
-
-#else
-    rep(i, n) {
-        ll hack_index = 0;
-        if (a.bucket_count() < 30000) {
-            hack_index = i;
-        } else {
-            hack_index = a.bucket_count() * max(0, min(i, 20000));
-        }
-        a[hack_index] = 1;
-        cout << i << " " << a.bucket_count() << " " <<  hack_index << endl;
+    for(int i = 0; i < 100000; ++i){
+        cout << s[i] << endl;
     }
-#endif
-
-    return 0;
 }
-
-
