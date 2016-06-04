615e447cc926459900950687c315c3d8bafe2d69
hamko <wakataberyo@gmail.com>
2016-06-04 23:08:14 +0200

    added ZAlgorithm

---
+++ library/ZAlgorithm.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(long long i = 0; i < (long long)(n); i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define mt make_tuple
+template<class T1, class T2> bool chmin(T1 &a, T2 b) { return b < a && (a = b, true); }
+template<class T1, class T2> bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+
+using ll = long long; using ld = long double; using vll = vector<ll>; using vvll = vector<vll>; using vld = vector<ld>; 
+using vi = vector<int>; using vvi = vector<vi>;
+using P = pair<ll, ll>;
+
+template <typename T, typename U> ostream &operator<<(ostream &o, const pair<T, U> &v) {  o << "(" << v.first << ", " << v.second << ")"; return o; }
+template<size_t...> struct seq{}; template<size_t N, size_t... Is> struct gen_seq : gen_seq<N-1, N-1, Is...>{}; template<size_t... Is> struct gen_seq<0, Is...> : seq<Is...>{};
+template<class Ch, class Tr, class Tuple, size_t... Is>
+void print_tuple(basic_ostream<Ch,Tr>& os, Tuple const& t, seq<Is...>){ using s = int[]; (void)s{0, (void(os << (Is == 0? "" : ", ") << get<Is>(t)), 0)...}; }
+template<class Ch, class Tr, class... Args> 
+auto operator<<(basic_ostream<Ch, Tr>& os, tuple<Args...> const& t) -> basic_ostream<Ch, Tr>& { os << "("; print_tuple(os, t, gen_seq<sizeof...(Args)>()); return os << ")"; }
+ostream &operator<<(ostream &o, const vvll &v) { rep(i, v.size()) { rep(j, v[i].size()) o << v[i][j] << " "; cout << endl; } return o; }
+template <typename T> ostream &operator<<(ostream &o, const vector<T> &v) { o << '['; rep(i, v.size()) o << v[i] << (i != v.size()-1 ? ", " : ""); o << "]";  return o; }
+template <typename T>  ostream &operator<<(ostream &o, const set<T> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T, typename U>  ostream &operator<<(ostream &o, const map<T, U> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T, typename U>  ostream &operator<<(ostream &o, const unordered_map<T, U> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it; o << "]";  return o; }
+#define VN(v) # v
+#define print(a) cout << a << "#" << VN(a) << endl;
+#define ldout fixed << setprecision(40) 
+
+// Z Algorithm
+// 構築 O(n)
+// 最長共通Suffix計算 O(1)
+//
+// 各iについて「S と S[i:|S|-1] の最長共通接頭辞の長さ」を計算
+class ZAlgorithm {
+public:
+    string s;
+    vector<ll> z;
+    
+    ZAlgorithm(string& s_) {
+        s = s_;
+        ll n = s.size();
+        z.assign(n, 0);
+
+        z[0] = n;
+        int i = 1, j = 0;
+        while (i < n) {
+            while (i+j < n && s[j] == s[i+j]) ++j;
+            z[i] = j;
+            if (j == 0) { ++i; continue;}
+            int k = 1;
+            while (i+k < n && k+z[k] < j) z[i+k] = z[k], ++k;
+            i += k; j -= k;
+        }
+    }
+    ll lcp(ll i) {
+        return z[i];
+    }
+};
+
+
+
+int main(void) {
+    cin.tie(0); ios::sync_with_stdio(false);
+    string s = "aaaabababbbb";
+
+    ZAlgorithm z(s);
+    cout << z.z << endl;
+}
