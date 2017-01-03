220ee5bd772b5a39d36b5ba3e342002bf1b633bd
hamko <wakataberyo@gmail.com>
2017-01-03 14:51:02 +0900

    added yukicoders

---
+++ yukicoder/1389/template.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef _WIN32
+#define scanfll(x) scanf("%I64d", x)
+#define printfll(x) printf("%I64d", x)
+#else
+#define scanfll(x) scanf("%lld", x)
+#define printfll(x) printf("%lld", x)
+#endif
+#define rep(i,n) for(long long i = 0; i < (long long)(n); i++)
+#define repi(i,a,b) for(long long i = (long long)(a); i < (long long)(b); i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define mt make_tuple
+#define mp make_pair
+template<class T1, class T2> bool chmin(T1 &a, T2 b) { return b < a && (a = b, true); }
+template<class T1, class T2> bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+
+using ll = long long; using vll = vector<ll>; using vvll = vector<vll>;
+using ld = long double;  using vld = vector<ld>; 
+using vi = vector<int>; using vvi = vector<vi>;
+vll conv(vi& v) { vll r(v.size()); rep(i, v.size()) r[i] = v[i]; return r; }
+using P = pair<ll, ll>;
+
+template <typename T, typename U> ostream &operator<<(ostream &o, const pair<T, U> &v) {  o << "(" << v.first << ", " << v.second << ")"; return o; }
+template<size_t...> struct seq{}; template<size_t N, size_t... Is> struct gen_seq : gen_seq<N-1, N-1, Is...>{}; template<size_t... Is> struct gen_seq<0, Is...> : seq<Is...>{};
+template<class Ch, class Tr, class Tuple, size_t... Is>
+void print_tuple(basic_ostream<Ch,Tr>& os, Tuple const& t, seq<Is...>){ using s = int[]; (void)s{0, (void(os << (Is == 0? "" : ", ") << get<Is>(t)), 0)...}; }
+template<class Ch, class Tr, class... Args> 
+auto operator<<(basic_ostream<Ch, Tr>& os, tuple<Args...> const& t) -> basic_ostream<Ch, Tr>& { os << "("; print_tuple(os, t, gen_seq<sizeof...(Args)>()); return os << ")"; }
+ostream &operator<<(ostream &o, const vvll &v) { rep(i, v.size()) { rep(j, v[i].size()) o << v[i][j] << " "; cout << endl; } return o; }
+template <typename T> ostream &operator<<(ostream &o, const vector<T> &v) { o << '['; rep(i, v.size()) o << v[i] << (i != v.size()-1 ? ", " : ""); o << "]";  return o; }
+template <typename T>  ostream &operator<<(ostream &o, const set<T> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T, typename U>  ostream &operator<<(ostream &o, const map<T, U> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T, typename U>  ostream &operator<<(ostream &o, const unordered_map<T, U> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it; o << "]";  return o; }
+string bits_to_string(ll mask, ll n) { string s; rep(i, n) s += '0' + !!(mask & (1ll << i)); return s; }
+#define ldout fixed << setprecision(40) 
+
+static const double EPS = 1e-14;
+static const long long INF = 1e18;
+
+static const long long mo = 1e9+7;
+class Mod {
+    public:
+        int num;
+        Mod() : Mod(0) {}
+        Mod(long long int n) : num(n) { }
+        Mod(const string &s){ long long int tmp = 0; for(auto &c:s) tmp = (c-'0'+tmp*10) % mo; num = tmp; }
+        Mod(int n) : Mod(static_cast<long long int>(n)) {}
+        operator int() { return num; }
+};
+istream &operator>>(istream &is, Mod &x) { long long int n; is >> n; x = n; return is; }
+ostream &operator<<(ostream &o, const Mod &x) { o << x.num; return o; }
+Mod operator+(const Mod a, const Mod b) { return Mod((a.num + b.num) % mo); }
+Mod operator+(const long long int a, const Mod b) { return Mod(a) + b; }
+Mod operator+(const Mod a, const long long int b) { return b + a; }
+Mod operator++(Mod &a) { return a + Mod(1); }
+Mod operator-(const Mod a, const Mod b) { return Mod((mo + a.num - b.num) % mo); }
+Mod operator-(const long long int a, const Mod b) { return Mod(a) - b; }
+Mod operator--(Mod &a) { return a - Mod(1); }
+Mod operator*(const Mod a, const Mod b) { return Mod(((long long)a.num * b.num) % mo); }
+Mod operator*(const long long int a, const Mod b) { return Mod(a)*b; }
+Mod operator*(const Mod a, const long long int b) { return Mod(b)*a; }
+Mod operator*(const Mod a, const int b) { return Mod(b)*a; }
+Mod operator+=(Mod &a, const Mod b) { return a = a + b; }
+Mod operator+=(long long int &a, const Mod b) { return a = a + b; }
+Mod operator-=(Mod &a, const Mod b) { return a = a - b; }
+Mod operator-=(long long int &a, const Mod b) { return a = a - b; }
+Mod operator*=(Mod &a, const Mod b) { return a = a * b; }
+Mod operator*=(long long int &a, const Mod b) { return a = a * b; }
+Mod operator*=(Mod& a, const long long int &b) { return a = a * b; }
+Mod factorial(const long long n) {
+    if (n < 0) return 0;
+    Mod ret = 1;
+    for (int i = 1; i <= n; i++) {
+        ret *= i;
+    }
+    return ret;
+}
+Mod operator^(const Mod a, const long long n) {
+    if (n == 0) return Mod(1);
+    Mod res = (a * a) ^ (n / 2);
+    if (n % 2) res = res * a;
+    return res;
+}
+Mod modpowsum(const Mod a, const long long b) {
+    if (b == 0) return 0;
+    if (b % 2 == 1) return modpowsum(a, b - 1) * a + Mod(1);
+    Mod result = modpowsum(a, b / 2);
+    return result * (a ^ (b / 2)) + result;
+}
+
+
+/*************************************/
+// 以下、modは素数でなくてはならない！
+/*************************************/
+Mod inv(const Mod a) { return a ^ (mo - 2); }
+Mod operator/(const Mod a, const Mod b) { assert(b.num != 0); return a * inv(b); }
+Mod operator/(const long long int a, const Mod b) { assert(b.num != 0); return Mod(a) * inv(b); }
+Mod operator/=(Mod &a, const Mod b) { assert(b.num != 0); return a = a * inv(b); }
+
+// n!と1/n!のテーブルを作る。
+// nCrを高速に計算するためのもの。
+//
+// assertでnを超えていないかをきちんとテストすること。
+//
+// O(n log mo)
+vector<Mod> fact, rfact;
+void constructFactorial(const long long n) {
+    fact.resize(n);
+    rfact.resize(n);
+    fact[0] = rfact[0] = 1;
+    for (int i = 1; i < n; i++) {
+        fact[i] = fact[i-1] * i;
+        rfact[i] = Mod(1) / fact[i];
+    }
+}
+
+int main(void) {
+    cin.tie(0); ios::sync_with_stdio(false);
+    constructFactorial(100);
+    ll n, k; cin >> n >> k;
+    assert(1 <= n && n <= 60);
+    assert(0 <= k && k <= n+1);
+
+    vll a(k); rep(i, a.size()) {
+        cin >> a[i];
+        assert(0 <= a[i]);
+        assert(a[i] <= ((1ll << n) - 1));
+    }
+
+    sort(all(a));
+    rep(i, k-1) {
+        rep(bit, 61) {
+            if ((a[i]&(1ll<<bit)) && !(a[i+1]&(1ll<<bit))) {
+                cout << 0 << endl;
+                return 0;
+            }
+        }
+    }
+
+    set<ll> popc;
+    popc.insert(0);
+    popc.insert(n);
+    rep(i, k) {
+        popc.insert(__builtin_popcountll(a[i]));
+    }
+    vll b; for (auto x : popc)  b.pb(x);
+
+    Mod ret = 1;
+    rep(i, b.size()-1) {
+        ret *= fact[b[i+1] - b[i]];
+    }
+    cout << ret << endl;
+    return 0;
+}
