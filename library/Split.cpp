60dab9ee0329805871583dc28b395b1decd48495
wakatabe <wakataberyo@gmail.com>
2017-02-02 15:26:52 +0900

    added default delim

--- library/Split.cpp
+++ library/Split.cpp
@@ -6,22 +6,31 @@ void split(const string &s, char delim, Out result) {
     stringstream ss;
     ss.str(s);
     string item;
-    while (getline(ss, item, delim)) {
-        *(result++) = item;
-    }
+    if (delim == ' ' || delim == '\t' || delim == '\n') 
+        while (ss >> item) 
+            *(result++) = item;
+    else 
+        while (getline(ss, item, delim)) 
+            *(result++) = item;
 }
 
 
-vector<string> split(const string &s, char delim) {
+vector<string> split(const string &s, char delim = ' ') {
     vector<string> elems;
     split(s, delim, back_inserter(elems));
     return elems;
 }
 
 int main(void) {
-    vector<string> ret = split("This is a test.", ' ');
-    for (auto s : ret) {
-        cout << s << endl;
+    {
+        vector<string> ret = split("This is a test, thanks");
+        for (auto s : ret) 
+            cout << s << endl;
+    }
+    {
+        vector<string> ret = split("This is a test, thanks", ',');
+        for (auto s : ret) 
+            cout << s << endl;
     }
     return 0;
 }
