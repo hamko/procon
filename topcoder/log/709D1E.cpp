4f8e5985ed358e3459a6d07763f95af754239ff2
Ryo Wakatabe <wakataberyo@gmail.com>
2017-02-22 04:19:12 +0900

    added 709D1E

---
+++ topcoder/log/709D1E.cpp
@@ -0,0 +1,262 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(long long i = 0; i < (long long)(n); i++)
+#define repi(i,a,b) for(long long i = (long long)(a); i < (long long)(b); i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define mt make_tuple
+#define mp make_pair
+template<class T1, class T2> bool chmin(T1 &a, T2 b) { return b < a && (a = b, true); }
+template<class T1, class T2> bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+#define exists find_if
+#define forall all_of
+
+using ll = long long; using vll = vector<ll>; using vvll = vector<vll>; using P = pair<ll, ll>;
+using ld = long double;  using vld = vector<ld>; 
+using vi = vector<int>; using vvi = vector<vi>; vll conv(vi& v) { vll r(v.size()); rep(i, v.size()) r[i] = v[i]; return r; }
+using Pos = complex<double>;
+
+namespace std{ namespace { template <class T> inline void hash_combine(size_t& seed, T const& v) { seed ^= hash<T>()(v) + 0x9e3779b9 + (seed<<6) + (seed>>2); } template <class Tuple, size_t Index = tuple_size<Tuple>::value - 1> struct HashValueImpl { static void apply(size_t& seed, Tuple const& tuple) { HashValueImpl<Tuple, Index-1>::apply(seed, tuple); hash_combine(seed, get<Index>(tuple)); } }; template <class Tuple> struct HashValueImpl<Tuple,0> { static void apply(size_t& seed, Tuple const& tuple) { hash_combine(seed, get<0>(tuple)); } }; } template <typename ... TT> struct hash<tuple<TT...>> { size_t operator()(tuple<TT...> const& tt) const { size_t seed = 0; HashValueImpl<tuple<TT...> >::apply(seed, tt); return seed; } }; } 
+namespace std { template<typename U, typename V> struct hash<pair<U, V>> { size_t operator()(pair<U, V> const& v) const { return v.first ^ v.second; } }; } struct pairhash { public: template <typename T, typename U> size_t operator()(const pair<T, U> &x) const { return hash<T>()(x.first) ^ hash<U>()(x.second); } };
+
+template <typename T, typename U> ostream &operator<<(ostream &o, const pair<T, U> &v) {  o << "(" << v.first << ", " << v.second << ")"; return o; }
+template<size_t...> struct seq{}; template<size_t N, size_t... Is> struct gen_seq : gen_seq<N-1, N-1, Is...>{}; template<size_t... Is> struct gen_seq<0, Is...> : seq<Is...>{};
+template<class Ch, class Tr, class Tuple, size_t... Is>
+void print_tuple(basic_ostream<Ch,Tr>& os, Tuple const& t, seq<Is...>){ using s = int[]; (void)s{0, (void(os << (Is == 0? "" : ", ") << get<Is>(t)), 0)...}; }
+template<class Ch, class Tr, class... Args> 
+auto operator<<(basic_ostream<Ch, Tr>& os, tuple<Args...> const& t) -> basic_ostream<Ch, Tr>& { os << "("; print_tuple(os, t, gen_seq<sizeof...(Args)>()); return os << ")"; }
+ostream &operator<<(ostream &o, const vvll &v) { rep(i, v.size()) { rep(j, v[i].size()) o << v[i][j] << " "; o << endl; } return o; }
+template <typename T> ostream &operator<<(ostream &o, const vector<T> &v) { o << '['; rep(i, v.size()) o << v[i] << (i != v.size()-1 ? ", " : ""); o << "]";  return o; }
+template <typename T>  ostream &operator<<(ostream &o, const set<T> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T, typename U>  ostream &operator<<(ostream &o, const map<T, U> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T, typename U>  ostream &operator<<(ostream &o, const unordered_map<T, U> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it; o << "]";  return o; }
+vector<int> range(const int x, const int y) { vector<int> v(y - x + 1); iota(v.begin(), v.end(), x); return v; }
+template <typename T> istream& operator>>(istream& i, vector<T>& o) { rep(j, o.size()) i >> o[j]; return i;}
+string bits_to_string(ll input, ll n=64) { string s; rep(i, n) s += '0' + !!(input & (1ll << i)); return s; }
+template <typename T> unordered_map<T, ll> counter(vector<T> vec){unordered_map<T, ll> ret; for (auto&& x : vec) ret[x]++; return ret;};
+string substr(string s, P x) {return s.substr(x.fi, x.se - x.fi); }
+struct ci : public iterator<forward_iterator_tag, ll> { ll n; ci(const ll n) : n(n) { } bool operator==(const ci& x) { return n == x.n; } bool operator!=(const ci& x) { return !(*this == x); } ci &operator++() { n++; return *this; } ll operator*() const { return n; } };
+
+static const double EPS = 1e-14;
+static const long long INF = 1e18;
+static const long long mo = 1e9+7;
+#define ldout fixed << setprecision(40) 
+
+class Xscoregame {
+    public:
+        int getscore(vector <int> a) {
+            vvll dp(1<<a.size(), vll(1<<6, -1));
+            dp[0][0] = 0;
+            rep(maski, 1<<a.size()) rep(j, 1<<6) if (dp[maski][j] >= 0) {
+                rep(i, a.size()) if (!(maski & (1 << i))) {
+                    ll next_val = dp[maski][j] + (dp[maski][j] ^ a[i]);
+                    chmax(dp[maski|(1<<i)][next_val%64], next_val);
+                }
+            }
+            ll ret = 0;
+            rep(j, 64) chmax(ret, dp[(1<<a.size())-1][j]);
+            return ret;
+        }
+};
+
+// BEGIN KAWIGIEDIT TESTING
+// Generated by KawigiEdit-pf 2.3.0
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cmath>
+using namespace std;
+bool KawigiEdit_RunTest(int testNum, vector <int> p0, bool hasAnswer, int p1) {
+	cout << "Test " << testNum << ": [" << "{";
+	for (int i = 0; int(p0.size()) > i; ++i) {
+		if (i > 0) {
+			cout << ",";
+		}
+		cout << p0[i];
+	}
+	cout << "}";
+	cout << "]" << endl;
+	Xscoregame *obj;
+	int answer;
+	obj = new Xscoregame();
+	clock_t startTime = clock();
+	answer = obj->getscore(p0);
+	clock_t endTime = clock();
+	delete obj;
+	bool res;
+	res = true;
+	cout << "Time: " << double(endTime - startTime) / CLOCKS_PER_SEC << " seconds" << endl;
+	if (hasAnswer) {
+		cout << "Desired answer:" << endl;
+		cout << "\t" << p1 << endl;
+	}
+	cout << "Your answer:" << endl;
+	cout << "\t" << answer << endl;
+	if (hasAnswer) {
+		res = answer == p1;
+	}
+	if (!res) {
+		cout << "DOESN'T MATCH!!!!" << endl;
+	} else if (double(endTime - startTime) / CLOCKS_PER_SEC >= 2) {
+		cout << "FAIL the timeout" << endl;
+		res = false;
+	} else if (hasAnswer) {
+		cout << "Match :-)" << endl;
+	} else {
+		cout << "OK, but is it right?" << endl;
+	}
+	cout << "" << endl;
+	return res;
+}
+int main() {
+	bool all_right;
+	bool disabled;
+	bool tests_disabled;
+	all_right = true;
+	tests_disabled = false;
+	
+	vector <int> p0;
+	int p1;
+	
+	// ----- test 0 -----
+	disabled = false;
+	p0 = {1,2,3};
+	p1 = 12;
+	all_right = (disabled || KawigiEdit_RunTest(0, p0, true, p1) ) && all_right;
+	tests_disabled = tests_disabled || disabled;
+	// ------------------
+		// ----- test 0 -----
+	disabled = false;
+	p0 = {13};
+	p1 = 26;
+	all_right = (disabled || KawigiEdit_RunTest(0, p0, true, p1) ) && all_right;
+	tests_disabled = tests_disabled || disabled;
+	// ------------------
+	
+	// ----- test 1 -----
+	disabled = false;
+	p0 = {10,0,0,0};
+	p1 = 80;
+	all_right = (disabled || KawigiEdit_RunTest(1, p0, true, p1) ) && all_right;
+	tests_disabled = tests_disabled || disabled;
+	// ------------------
+	
+	// ----- test 2 -----
+	disabled = false;
+	p0 = {1,1,1,1,1,1};
+	p1 = 1;
+	all_right = (disabled || KawigiEdit_RunTest(2, p0, true, p1) ) && all_right;
+	tests_disabled = tests_disabled || disabled;
+	// ------------------
+	
+	// ----- test 3 -----
+	disabled = false;
+	p0 = {1,0,1,0,1,0,1,0};
+	p1 = 170;
+	all_right = (disabled || KawigiEdit_RunTest(3, p0, true, p1) ) && all_right;
+	tests_disabled = tests_disabled || disabled;
+	// ------------------
+	
+	// ----- test 4 -----
+	disabled = false;
+	p0 = {50,0,1,0,1,0,1,0,1,0,1,0,1,0,1};
+	p1 = 830122;
+	all_right = (disabled || KawigiEdit_RunTest(4, p0, true, p1) ) && all_right;
+	tests_disabled = tests_disabled || disabled;
+	// ------------------
+		// ----- test 4 -----
+	disabled = false;
+	p0 = {0};
+	p1 = 0;
+	all_right = (disabled || KawigiEdit_RunTest(4, p0, true, p1) ) && all_right;
+	tests_disabled = tests_disabled || disabled;
+	// ------------------
+	
+	if (all_right) {
+		if (tests_disabled) {
+			cout << "You're a stud (but some test cases were disabled)!" << endl;
+		} else {
+			cout << "You're a stud (at least on given cases)!" << endl;
+		}
+	} else {
+		cout << "Some of the test cases had errors." << endl;
+	}
+	return 0;
+}
+// PROBLEM STATEMENT
+// You have an array of integers: the vector <int> A.
+// 
+// When you give the array to Hero, he will execute the following steps:
+// 
+// At the beginning, he will set the variable X to zero.
+// For each element Y of A, in the given order, he will set X to (X + (X xor Y)).
+// He will announce the final value of the variable X.
+// 
+// 
+// Before you give the array A to Hero, you are allowed to permute its elements arbitrarily.
+// Compute and return the largest possible value that can be announced by Hero.
+// 
+// DEFINITION
+// Class:Xscoregame
+// Method:getscore
+// Parameters:vector <int>
+// Returns:int
+// Method signature:int getscore(vector <int> A)
+// 
+// 
+// CONSTRAINTS
+// -A will contain between 1 and 15 elements, inclusive.
+// -Each element in A will be between 0 and 50, inclusive.
+// 
+// 
+// EXAMPLES
+// 
+// 0)
+// {1,2,3}
+// 
+// Returns: 12
+// 
+// An optimal solution is to rearrange A into the order {3, 1, 2}.
+// Given this array, Hero will perform the following steps:
+// 
+// He will set X to zero.
+// Then, he will set X to (0 + (0 xor 3)) = 3.
+// Then, he will set X to (3 + (3 xor 1)) = 5.
+// Finally, he will set X to (5 + (5 xor 2)) = 12, and he will announce the value 12.
+// 
+// 
+// 1)
+// {10,0,0,0}
+// 
+// Returns: 80
+// 
+// Here an optimal solution is to leave the array in the original order.
+// The variable X will have the values 0, 10, 20, 40, and finally 80.
+// 
+// 2)
+// {1,1,1,1,1,1}
+// 
+// Returns: 1
+// 
+// 
+// 
+// 3)
+// {1,0,1,0,1,0,1,0}
+// 
+// Returns: 170
+// 
+// 
+// 
+// 4)
+// {50,0,1,0,1,0,1,0,1,0,1,0,1,0,1}
+// 
+// Returns: 830122
+// 
+// 
+// 
+// END KAWIGIEDIT TESTING
+//Powered by KawigiEdit-pf 2.3.0!
