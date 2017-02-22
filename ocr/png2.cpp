920536c5c5d7800836a952478a359f46f2cfb508
wakatabe <wakataberyo@gmail.com>
2017-02-22 14:10:19 +0900

    added png

---
+++ ocr/png2.cpp
@@ -0,0 +1,327 @@
+/*
+ * Copyright 2002-2010 Guillaume Cottenceau.
+ *
+ * This software may be freely redistributed under the terms
+ * of the X11 license.
+ *
+ */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <iostream>
+#include <vector>
+#include <tuple>
+using namespace std;
+
+#define PNG_DEBUG 3
+#include <png.h>
+
+void abort_(const char * s, ...)
+{
+        va_list args;
+        va_start(args, s);
+        vfprintf(stderr, s, args);
+        fprintf(stderr, "\n");
+        va_end(args);
+        abort();
+}
+
+int x, y;
+
+int width, height;
+png_byte color_type;
+png_byte bit_depth;
+
+png_structp png_ptr;
+png_infop info_ptr;
+int number_of_passes;
+png_bytep * row_pointers; // row_pointers[y][x]
+
+void read_png_file(char* file_name)
+{
+        char header[8];    // 8 is the maximum size that can be checked
+
+        /* open file and test for it being a png */
+        FILE *fp = fopen(file_name, "rb");
+        if (!fp)
+                abort_("[read_png_file] File %s could not be opened for reading", file_name);
+        fread(header, 1, 8, fp);
+        if (png_sig_cmp((png_byte*)header, 0, 8))
+                abort_("[read_png_file] File %s is not recognized as a PNG file", file_name);
+
+
+        /* initialize stuff */
+        png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+        if (!png_ptr)
+                abort_("[read_png_file] png_create_read_struct failed");
+
+        info_ptr = png_create_info_struct(png_ptr);
+        if (!info_ptr)
+                abort_("[read_png_file] png_create_info_struct failed");
+
+        if (setjmp(png_jmpbuf(png_ptr)))
+                abort_("[read_png_file] Error during init_io");
+
+        png_init_io(png_ptr, fp);
+        png_set_sig_bytes(png_ptr, 8);
+
+        png_read_info(png_ptr, info_ptr);
+
+        width = png_get_image_width(png_ptr, info_ptr);
+        height = png_get_image_height(png_ptr, info_ptr);
+        color_type = png_get_color_type(png_ptr, info_ptr);
+        bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+
+        number_of_passes = png_set_interlace_handling(png_ptr);
+        png_read_update_info(png_ptr, info_ptr);
+
+
+        /* read file */
+        if (setjmp(png_jmpbuf(png_ptr)))
+                abort_("[read_png_file] Error during read_image");
+
+        row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
+        for (y=0; y<height; y++)
+                row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
+
+        png_read_image(png_ptr, row_pointers);
+
+        fclose(fp);
+}
+
+
+void write_png_file(char* file_name)
+{
+        /* create file */
+        FILE *fp = fopen(file_name, "wb");
+        if (!fp)
+                abort_("[write_png_file] File %s could not be opened for writing", file_name);
+
+
+        /* initialize stuff */
+        png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+        if (!png_ptr)
+                abort_("[write_png_file] png_create_write_struct failed");
+
+        info_ptr = png_create_info_struct(png_ptr);
+        if (!info_ptr)
+                abort_("[write_png_file] png_create_info_struct failed");
+
+        if (setjmp(png_jmpbuf(png_ptr)))
+                abort_("[write_png_file] Error during init_io");
+
+        png_init_io(png_ptr, fp);
+
+
+        /* write header */
+        if (setjmp(png_jmpbuf(png_ptr)))
+                abort_("[write_png_file] Error during writing header");
+
+        png_set_IHDR(png_ptr, info_ptr, width, height,
+                     bit_depth, color_type, PNG_INTERLACE_NONE,
+                     PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+        png_write_info(png_ptr, info_ptr);
+
+
+        /* write bytes */
+        if (setjmp(png_jmpbuf(png_ptr)))
+                abort_("[write_png_file] Error during writing bytes");
+
+        png_write_image(png_ptr, row_pointers);
+
+
+        /* end write */
+        if (setjmp(png_jmpbuf(png_ptr)))
+                abort_("[write_png_file] Error during end of write");
+
+        png_write_end(png_ptr, NULL);
+
+        /* cleanup heap allocation */
+        for (y=0; y<height; y++)
+                free(row_pointers[y]);
+        free(row_pointers);
+
+        fclose(fp);
+}
+
+
+void process_file_rgba(void)
+{
+        for (y=0; y<height; y++) {
+                png_byte* row = row_pointers[y];
+                for (x=0; x<width; x++) {
+                        png_byte* ptr = &(row[x*4]);
+                        printf("Pixel at position [ %d - %d ] has RGBA values: %d - %d - %d - %d\n",
+                               x, y, ptr[0], ptr[1], ptr[2], ptr[3]);
+
+                        /* set red value to 0 and green value to the blue one */
+                        ptr[0] = 0;
+                        ptr[1] = ptr[2];
+                }
+        }
+
+}
+
+void process_file_rgb(void)
+{
+        for (y=0; y<height; y++) {
+                png_byte* row = row_pointers[y];
+                for (x=0; x<width; x++) {
+                        png_byte* ptr = &(row[x*3]);
+                        printf("Pixel at position [ %d - %d ] has RGB values: %d - %d - %d\n",
+                               x, y, ptr[0], ptr[1], ptr[2]);
+
+                        /* set red value to 0 and green value to the blue one */
+                        ptr[0] = 0;
+                        ptr[1] = ptr[2];
+                }
+        }
+
+}
+
+void process_file(void)
+{
+        if (png_get_color_type(png_ptr, info_ptr) == PNG_COLOR_TYPE_RGB) {
+            process_file_rgb();
+        } else if (png_get_color_type(png_ptr, info_ptr) == PNG_COLOR_TYPE_RGBA) {
+            process_file_rgba();
+        } else {
+                abort_("[process_file] color_type of input file must be PNG_COLOR_TYPE_RGBA (%d) (is %d)",
+                       PNG_COLOR_TYPE_RGBA, png_get_color_type(png_ptr, info_ptr));
+        }
+}
+
+#define rep(i,n) for(long long i = 0; i < (long long)(n); i++)
+#define repi(i,a,b) for(long long i = (long long)(a); i < (long long)(b); i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+using ll = long long; using vll = vector<ll>; using vvll = vector<vll>; using P = pair<ll, ll>;
+using ld = long double;  using vld = vector<ld>; 
+using vi = vector<int>; using vvi = vector<vi>; vll conv(vi& v) { vll r(v.size()); rep(i, v.size()) r[i] = v[i]; return r; }
+template <typename T, typename U> ostream &operator<<(ostream &o, const pair<T, U> &v) {  o << "(" << v.first << ", " << v.second << ")"; return o; }
+template<size_t...> struct seq{}; template<size_t N, size_t... Is> struct gen_seq : gen_seq<N-1, N-1, Is...>{}; template<size_t... Is> struct gen_seq<0, Is...> : seq<Is...>{};
+template<class Ch, class Tr, class Tuple, size_t... Is>
+void print_tuple(basic_ostream<Ch,Tr>& os, Tuple const& t, seq<Is...>){ using s = int[]; (void)s{0, (void(os << (Is == 0? "" : ", ") << get<Is>(t)), 0)...}; }
+template<class Ch, class Tr, class... Args> 
+auto operator<<(basic_ostream<Ch, Tr>& os, tuple<Args...> const& t) -> basic_ostream<Ch, Tr>& { os << "("; print_tuple(os, t, gen_seq<sizeof...(Args)>()); return os << ")"; }
+ostream &operator<<(ostream &o, const vvll &v) { rep(i, v.size()) { rep(j, v[i].size()) o << v[i][j] << " "; o << endl; } return o; }
+template <typename T> ostream &operator<<(ostream &o, const vector<T> &v) { o << '['; rep(i, v.size()) o << v[i] << (i != v.size()-1 ? ", " : ""); o << "]";  return o; }
+
+// 添字は行列のイメージ
+using image_t = vector<vector<tuple<int, int, int>>>;
+image_t getBlackImage(int h, int w) {
+    return image_t(h, vector<tuple<int, int, int>>(w));
+}
+image_t getImage(string filename) {
+    image_t image;
+    read_png_file((char*)filename.c_str());
+    image.resize(height);
+    rep(i, height) image[i].resize(width);
+
+    for (y=0; y<height; y++) {
+        png_byte* row = row_pointers[y];
+        for (x=0; x<width; x++) {
+            png_byte* ptr = &(row[x*3]);
+            image[y][x] = tie(ptr[0], ptr[1], ptr[2]);
+        }
+    }
+    return image;
+}
+
+// 添字は行列のイメージ
+void setImage(string filename, image_t image) {
+    int h = image.size(), w = image[0].size();
+    row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * h);
+    for (y=0; y<h; y++)
+        row_pointers[y] = (png_byte*) malloc(sizeof(png_ptr) * w);
+
+    for (y=0; y<h; y++) {
+        for (x=0; x<w; x++) {
+            png_byte* ptr = &row_pointers[y][x*3];
+            tie(ptr[0], ptr[1], ptr[2]) = image[y][x];
+        }
+    }
+
+    /* create file */
+    FILE *fp = fopen(filename.c_str(), "wb");
+    if (!fp) abort_("[write_png_file] File %s could not be opened for writing", filename.c_str());
+
+
+    /* initialize stuff */
+    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+    if (!png_ptr)
+        abort_("[write_png_file] png_create_write_struct failed");
+
+    info_ptr = png_create_info_struct(png_ptr);
+    if (!info_ptr)
+        abort_("[write_png_file] png_create_info_struct failed");
+
+    if (setjmp(png_jmpbuf(png_ptr)))
+        abort_("[write_png_file] Error during init_io");
+
+    png_init_io(png_ptr, fp);
+
+
+    /* write header */
+    if (setjmp(png_jmpbuf(png_ptr)))
+        abort_("[write_png_file] Error during writing header");
+
+    png_set_IHDR(png_ptr, info_ptr, w, h,
+            bit_depth, color_type, PNG_INTERLACE_NONE,
+            PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+    png_write_info(png_ptr, info_ptr);
+
+
+    /* write bytes */
+    if (setjmp(png_jmpbuf(png_ptr)))
+        abort_("[write_png_file] Error during writing bytes");
+
+    png_write_image(png_ptr, row_pointers);
+
+
+    /* end write */
+    if (setjmp(png_jmpbuf(png_ptr)))
+        abort_("[write_png_file] Error during end of write");
+
+    png_write_end(png_ptr, NULL);
+
+    /* cleanup heap allocation */
+    for (y=0; y<h; y++)
+        free(row_pointers[y]);
+    free(row_pointers);
+
+    fclose(fp);
+}
+
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+        abort_("Usage: program_name <file_in> <file_out>");
+
+    cout << "Upper-Left is the origin. " << endl;
+    cout << "x ------------> " << endl;
+    cout << "y" << endl;
+    cout << "|" << endl;
+    cout << "v" << endl;
+
+    auto image = getImage(string(argv[1]));
+    rep(y, image.size()) rep(x, image[h].size()) {
+        int r, g, b; tie(r, g, b) = image[y][x];
+        cout << y << " " << x << " : " << r << " " << g << " " << b << endl;
+    }
+
+    setImage(string(argv[2]), getBlackImage(50, 50));
+
+
+    return 0;
+}
+
