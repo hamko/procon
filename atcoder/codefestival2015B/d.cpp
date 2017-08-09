65590c4da683f1f90cfa85b89dd6f0751d4eae96
Ryo Wakatabe <wakataberyo@gmail.com>
2017-08-09 14:23:05 +0900

    added 603

--- atcoder/codefestival2015B/d.cpp
+++ atcoder/codefestival2015B/d.cpp
@@ -1,13 +1,7 @@
 #include <bits/stdc++.h>
+#include <sys/time.h>
 using namespace std;
 
-#ifdef _WIN32
-#define scanfll(x) scanf("%I64d", x)
-#define printfll(x) printf("%I64d", x)
-#else
-#define scanfll(x) scanf("%lld", x)
-#define printfll(x) printf("%lld", x)
-#endif
 #define rep(i,n) for(long long i = 0; i < (long long)(n); i++)
 #define repi(i,a,b) for(long long i = (long long)(a); i < (long long)(b); i++)
 #define pb push_back
@@ -16,13 +10,16 @@ using namespace std;
 #define se second
 #define mt make_tuple
 #define mp make_pair
+#define ZERO(a) memset(a,0,sizeof(a))
 template<class T1, class T2> bool chmin(T1 &a, T2 b) { return b < a && (a = b, true); }
 template<class T1, class T2> bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+#define exists find_if
+#define forall all_of
 
-using ll = long long; using ld = long double; using vll = vector<ll>; using vvll = vector<vll>; using vld = vector<ld>; 
-using vi = vector<int>; using vvi = vector<vi>;
-vll conv(vi& v) { vll r(v.size()); rep(i, v.size()) r[i] = v[i]; return r; }
-using P = pair<ll, ll>;
+using ll = long long; using vll = vector<ll>; using vvll = vector<vll>; using P = pair<ll, ll>;
+using ld = long double;  using vld = vector<ld>; 
+using vi = vector<int>; using vvi = vector<vi>; vll conv(vi& v) { vll r(v.size()); rep(i, v.size()) r[i] = v[i]; return r; }
+using Pos = complex<double>;
 
 template <typename T, typename U> ostream &operator<<(ostream &o, const pair<T, U> &v) {  o << "(" << v.first << ", " << v.second << ")"; return o; }
 template<size_t...> struct seq{}; template<size_t N, size_t... Is> struct gen_seq : gen_seq<N-1, N-1, Is...>{}; template<size_t... Is> struct gen_seq<0, Is...> : seq<Is...>{};
@@ -30,74 +27,50 @@ template<class Ch, class Tr, class Tuple, size_t... Is>
 void print_tuple(basic_ostream<Ch,Tr>& os, Tuple const& t, seq<Is...>){ using s = int[]; (void)s{0, (void(os << (Is == 0? "" : ", ") << get<Is>(t)), 0)...}; }
 template<class Ch, class Tr, class... Args> 
 auto operator<<(basic_ostream<Ch, Tr>& os, tuple<Args...> const& t) -> basic_ostream<Ch, Tr>& { os << "("; print_tuple(os, t, gen_seq<sizeof...(Args)>()); return os << ")"; }
-ostream &operator<<(ostream &o, const vvll &v) { rep(i, v.size()) { rep(j, v[i].size()) o << v[i][j] << " "; cout << endl; } return o; }
+ostream &operator<<(ostream &o, const vvll &v) { rep(i, v.size()) { rep(j, v[i].size()) o << v[i][j] << " "; o << endl; } return o; }
 template <typename T> ostream &operator<<(ostream &o, const vector<T> &v) { o << '['; rep(i, v.size()) o << v[i] << (i != v.size()-1 ? ", " : ""); o << "]";  return o; }
 template <typename T>  ostream &operator<<(ostream &o, const set<T> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T>  ostream &operator<<(ostream &o, const unordered_set<T> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
 template <typename T, typename U>  ostream &operator<<(ostream &o, const map<T, U> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
-template <typename T, typename U>  ostream &operator<<(ostream &o, const unordered_map<T, U> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it; o << "]";  return o; }
-void printbits(ll mask, ll n) { rep(i, n) { cout << !!(mask & (1ll << i)); } cout << endl; }
-#define ldout fixed << setprecision(40) 
+template <typename T, typename U, typename V>  ostream &operator<<(ostream &o, const unordered_map<T, U, V> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it; o << "]";  return o; }
+vector<int> range(const int x, const int y) { vector<int> v(y - x + 1); iota(v.begin(), v.end(), x); return v; }
+template <typename T> istream& operator>>(istream& i, vector<T>& o) { rep(j, o.size()) i >> o[j]; return i;}
+string bits_to_string(ll input, ll n=64) { string s; rep(i, n) s += '0' + !!(input & (1ll << i)); reverse(all(s)); return s; }
+
+template <typename T> unordered_map<T, ll> counter(vector<T> vec){unordered_map<T, ll> ret; for (auto&& x : vec) ret[x]++; return ret;};
+string substr(string s, P x) {return s.substr(x.fi, x.se - x.fi); }
+struct ci : public iterator<forward_iterator_tag, ll> { ll n; ci(const ll n) : n(n) { } bool operator==(const ci& x) { return n == x.n; } bool operator!=(const ci& x) { return !(*this == x); } ci &operator++() { n++; return *this; } ll operator*() const { return n; } };
+
+size_t random_seed; namespace std { using argument_type = P; template<> struct hash<argument_type> { size_t operator()(argument_type const& x) const { size_t seed = random_seed; seed ^= hash<ll>{}(x.fi); seed ^= (hash<ll>{}(x.se) << 1); return seed; } }; }; // hash for various class
+namespace myhash{ const int Bsizes[]={3,9,13,17,21,25,29,33,37,41,45,49,53,57,61,65,69,73,77,81}; const int xor_nums[]={0x100007d1,0x5ff049c9,0x14560859,0x07087fef,0x3e277d49,0x4dba1f17,0x709c5988,0x05904258,0x1aa71872,0x238819b3,0x7b002bb7,0x1cf91302,0x0012290a,0x1083576b,0x76473e49,0x3d86295b,0x20536814,0x08634f4d,0x115405e8,0x0e6359f2}; const int hash_key=xor_nums[rand()%20]; const int mod_key=xor_nums[rand()%20]; template <typename T> struct myhash{ std::size_t operator()(const T& val) const { return (hash<T>{}(val)%mod_key)^hash_key; } }; };
+template <typename T> class uset:public std::unordered_set<T,myhash::myhash<T>> { using SET=std::unordered_set<T,myhash::myhash<T>>; public: uset():SET(){SET::rehash(myhash::Bsizes[rand()%20]);} };
+template <typename T,typename U> class umap:public std::unordered_map<T,U,myhash::myhash<T>> { public: using MAP=std::unordered_map<T,U,myhash::myhash<T>>; umap():MAP(){MAP::rehash(myhash::Bsizes[rand()%20]);} };    
+
+struct timeval start; double sec() { struct timeval tv; gettimeofday(&tv, NULL); return (tv.tv_sec - start.tv_sec) + (tv.tv_usec - start.tv_usec) * 1e-6; }
+struct init_{init_(){ gettimeofday(&start, NULL); ios::sync_with_stdio(false); cin.tie(0); srand((unsigned int)time(NULL)); random_seed = RAND_MAX / 2 + rand() / 2; }} init__;
 
 static const double EPS = 1e-14;
 static const long long INF = 1e18;
 static const long long mo = 1e9+7;
+#define ldout fixed << setprecision(40) 
 
 int main(void) {
-    cin.tie(0); ios::sync_with_stdio(false);
     ll n; cin >> n;
-    vll s(n), c(n); rep(i, s.size()) cin >> s[i] >> c[i];
     set<P> memo;
-    set<P, greater<P>> memo_rev;
-    vector<ll> ret(n);
     rep(i, n) {
-        cout << memo << endl;
-        for (auto x : memo_rev) { cout << x << " "; } cout << endl;
-        ll start_point = s[i];
-
-        ll pos = s[i]; // 塗ろうとしている位置
-        ll cost = c[i];
-        vector<set<P>::iterator> to_erase;
-        vector<set<P, greater<P>>::iterator> to_erase_rev;
-        while (cost) {
-            // pos以上の最小の終端位置
-            auto l = upper_bound(all(memo), P(pos, -1));
-            // pos以下の最大の終端位置
-            auto r = upper_bound(all(memo_rev), P(pos, INF));
-
-            cout << pos << " " << cost << endl;
-            cout << (l == memo.end() ? P(-1, -1) : *l) << "#l" << endl;
-            cout << (r == memo_rev.end() ? P(-1, -1) : *r) << "#r" << endl;
-
-            if (l == memo.end() && r == memo_rev.end()) {
-                pos += cost;
-                cost = 0;
-            } else if (l != r) {
-                auto next_r = memo.upper_bound(P(pos, -1));
-                if (next_r != memo.end() && next_r->fi <= pos + cost) {
-                    to_erase.pb(next_r);
-                    to_erase_rev.pb(memo_rev.find(*next_r));
-                    ll used_cost = next_r->fi - pos;
-                    cost -= used_cost;
-                    pos += used_cost;
-                } else {
-                    memo.insert(P(pos, pos+cost-1));
-                    memo_rev.insert(P(pos, pos+cost-1));
-                    pos += cost;
-                    cost = 0;
-                }
+        ll l, len; cin >> l >> len;
+        P query = P(l+len-1, l);
+        while (1) {
+            auto it = memo.lower_bound(P(query.se, -INF));
+            if (it == memo.end() || (it->se) > query.fi) {
+                memo.insert(query);
+                cout << query.fi << endl;
+                break;
             } else {
-                pos = l->se + 1; 
+                memo.erase(it);
+                query = P(min(query.se, it->se) + (query.fi-query.se+1) + (it->fi-it->se+1) - 1, min(query.se, it->se));
             }
         }
-        for (auto x : to_erase) {
-            memo.erase(x);
-        }
-        memo.insert(P(start_point, pos-1));
-        ret[i] = pos-1;
-    }
-
-    rep(i, n) {
-        cout << ret[i] << endl;
     }
     return 0;
 }
