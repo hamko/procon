0f62f333447d77f666b0452bea05fd3f6c263df8
Ryo Wakatabe <wakataberyo@gmail.com>
2017-09-22 18:59:09 +0900

    fixed stack -> queue

--- pck/pck2017/9.cpp
+++ pck/pck2017/9.cpp
@@ -149,8 +149,9 @@ int main(void) {
     rep(i, n) used.add(i, i+1, 1);
 
     stack<ll> pos[256];
+//    rep(i, n) 
     rep(i, n) 
-        pos[s[i]].push(i);
+        pos[s[n-i-1]].push(n-i-1);
 
     string ret;
     ll cur = 0;
