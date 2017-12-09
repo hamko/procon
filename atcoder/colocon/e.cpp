6997ec3d365bc465cfbea4e43b396f39286f8bb5
Ryo Wakatabe <wakataberyo@gmail.com>
2017-12-10 03:32:47 +0900

    added

---
+++ atcoder/colocon/e.cpp
@@ -0,0 +1,191 @@
+#include <bits/stdc++.h>
+#include <sys/time.h>
+using namespace std;
+
+#define rep(i,n) for(long long i = 0; i < (long long)(n); i++)
+#define repi(i,a,b) for(long long i = (long long)(a); i < (long long)(b); i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define mt make_tuple
+#define mp make_pair
+template<class T1, class T2> bool chmin(T1 &a, T2 b) { return b < a && (a = b, true); }
+template<class T1, class T2> bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+
+using ll = long long; using vll = vector<ll>; using vvll = vector<vll>; using P = pair<ll, ll>;
+using ld = long double;  using vld = vector<ld>; 
+using vi = vector<int>; using vvi = vector<vi>; vll conv(vi& v) { vll r(v.size()); rep(i, v.size()) r[i] = v[i]; return r; }
+
+inline void input(int &v){ v=0;char c=0;int p=1; while(c<'0' || c>'9'){if(c=='-')p=-1;c=getchar();} while(c>='0' && c<='9'){v=(v<<3)+(v<<1)+c-'0';c=getchar();} v*=p; }
+template <typename T, typename U> ostream &operator<<(ostream &o, const pair<T, U> &v) {  o << "(" << v.first << ", " << v.second << ")"; return o; }
+template<size_t...> struct seq{}; template<size_t N, size_t... Is> struct gen_seq : gen_seq<N-1, N-1, Is...>{}; template<size_t... Is> struct gen_seq<0, Is...> : seq<Is...>{};
+template<class Ch, class Tr, class Tuple, size_t... Is>
+void print_tuple(basic_ostream<Ch,Tr>& os, Tuple const& t, seq<Is...>){ using s = int[]; (void)s{0, (void(os << (Is == 0? "" : ", ") << get<Is>(t)), 0)...}; }
+template<class Ch, class Tr, class... Args> 
+auto operator<<(basic_ostream<Ch, Tr>& os, tuple<Args...> const& t) -> basic_ostream<Ch, Tr>& { os << "("; print_tuple(os, t, gen_seq<sizeof...(Args)>()); return os << ")"; }
+ostream &operator<<(ostream &o, const vvll &v) { rep(i, v.size()) { rep(j, v[i].size()) o << v[i][j] << " "; o << endl; } return o; }
+template <typename T> ostream &operator<<(ostream &o, const vector<T> &v) { o << '['; rep(i, v.size()) o << v[i] << (i != v.size()-1 ? ", " : ""); o << "]";  return o; }
+template <typename T> ostream &operator<<(ostream &o, const deque<T> &v) { o << '['; rep(i, v.size()) o << v[i] << (i != v.size()-1 ? ", " : ""); o << "]";  return o; }
+template <typename T>  ostream &operator<<(ostream &o, const set<T> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T>  ostream &operator<<(ostream &o, const unordered_set<T> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T, typename U>  ostream &operator<<(ostream &o, const map<T, U> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T, typename U, typename V>  ostream &operator<<(ostream &o, const unordered_map<T, U, V> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it; o << "]";  return o; }
+vector<int> range(const int x, const int y) { vector<int> v(y - x + 1); iota(v.begin(), v.end(), x); return v; }
+template <typename T> istream& operator>>(istream& i, vector<T>& o) { rep(j, o.size()) i >> o[j]; return i;}
+template <typename T, typename S, typename U> ostream &operator<<(ostream &o, const priority_queue<T, S, U> &v) { auto tmp = v; while (tmp.size()) { auto x = tmp.top(); tmp.pop(); o << x << " ";} return o; }
+template <typename T> ostream &operator<<(ostream &o, const queue<T> &v) { auto tmp = v; while (tmp.size()) { auto x = tmp.front(); tmp.pop(); o << x << " ";} return o; }
+template <typename T> ostream &operator<<(ostream &o, const stack<T> &v) { auto tmp = v; while (tmp.size()) { auto x = tmp.top(); tmp.pop(); o << x << " ";} return o; }
+template <typename T> unordered_map<T, ll> counter(vector<T> vec){unordered_map<T, ll> ret; for (auto&& x : vec) ret[x]++; return ret;};
+string substr(string s, P x) {return s.substr(x.fi, x.se - x.fi); }
+void vizGraph(vvll& g, int mode = 0, string filename = "out.png") { ofstream ofs("./out.dot"); ofs << "digraph graph_name {" << endl; set<P> memo; rep(i, g.size())  rep(j, g[i].size()) { if (mode && (memo.count(P(i, g[i][j])) || memo.count(P(g[i][j], i)))) continue; memo.insert(P(i, g[i][j])); ofs << "    " << i << " -> " << g[i][j] << (mode ? " [arrowhead = none]" : "")<< endl;  } ofs << "}" << endl; ofs.close(); system(((string)"dot -T png out.dot >" + filename).c_str()); }
+size_t random_seed; namespace std { using argument_type = P; template<> struct hash<argument_type> { size_t operator()(argument_type const& x) const { size_t seed = random_seed; seed ^= hash<ll>{}(x.fi); seed ^= (hash<ll>{}(x.se) << 1); return seed; } }; }; // hash for various class
+struct timeval start; double sec() { struct timeval tv; gettimeofday(&tv, NULL); return (tv.tv_sec - start.tv_sec) + (tv.tv_usec - start.tv_usec) * 1e-6; }
+struct init_{init_(){ ios::sync_with_stdio(false); cin.tie(0); gettimeofday(&start, NULL); struct timeval myTime; struct tm *time_st; gettimeofday(&myTime, NULL); time_st = localtime(&myTime.tv_sec); srand(myTime.tv_usec); random_seed = RAND_MAX / 2 + rand() / 2; }} init__;
+uint32_t randxor() { static uint32_t x=1+(uint32_t)random_seed,y=362436069,z=521288629,w=88675123; uint32_t t; t=(x^(x<<11));x=y;y=z;z=w; return( w=(w^(w>>19))^(t^(t>>8)) ); }
+#define rand randxor
+#define ldout fixed << setprecision(40) 
+
+#define EPS (double)1e-14
+#define INF (ll)1e18
+#define mo  (ll)(1e9+7)
+
+ll k;
+ll calc(ll n, ll m) {
+    if (n % 2 == 0) return INF;
+    if (m % 2 == 0) return INF;
+    ll x = k - n * m - m - 1;
+    return (x>=0?4+2*(m+n+x):INF);
+}
+
+vector<P> traj;
+#define DISP (traj.pb(P(x, y)))
+
+void solveSmall() {
+    ll x = 0, y = 0;
+    DISP;
+    x++;
+    DISP;
+
+    ll dir = -1;
+    rep(i, k-2) {
+        y++;
+        DISP;
+        x += dir*2;
+        DISP;
+        dir *= -1;
+    }
+    y++;
+    DISP;
+    x += dir * 1;
+    DISP;
+
+    rep(i, traj.size()-1) {
+        assert(traj[i].fi == traj[i+1].fi || traj[i].se == traj[i+1].se );
+    }
+    cout << traj.size() << endl;
+    for (auto hoge : traj) {
+        cout << hoge.fi << " " << hoge.se << endl;
+    }
+}
+
+int main(void) {
+    cin >> k;
+    if (k == 2) {
+        cout << 4 << endl;
+        cout << 0 << " " << 0 << endl;
+        cout << 1 << " " << 0 << endl;
+        cout << 1 << " " << 1 << endl;
+        cout << 0 << " " << 1 << endl;
+        return 0;
+    }
+    if (k < 20) {
+        solveSmall();
+        return 0;
+    }
+
+    ll good = INF;
+    ll n0 = -1, m0 = -1;
+    rep(n, 2000) rep(m, 2000) {
+        ll tmp = calc(n, m);
+//        cout << n << " " << m << " " <<  tmp << endl;
+        if (good > tmp) {
+            good = tmp;
+            n0 = n;
+            m0 = m;
+        }
+    }
+//    cout << good << endl;
+    ll n = n0, m = m0;
+
+//    cout << n << " " << m << endl;
+
+    ll x = 0, y = 0;
+    ll dir = -1;
+    rep(i, n) {
+        DISP;
+        y += (m+1)*dir;
+        DISP;
+        x++;
+        dir *= -1;
+    }
+
+    DISP;
+    y+=m;
+    DISP;
+    x = -1;
+    DISP;
+    y--;
+
+    dir = 1;
+    rep(i, m-1) {
+        DISP;
+        x += dir * (n + 2);
+        DISP;
+        y--;
+        dir *= -1;
+    }
+    y++;
+
+    x--;
+    DISP;
+    y = 0;
+    DISP;
+
+    x++;
+    DISP;
+    assert(x == -1 && y == 0);
+
+
+    y++;
+    DISP; // (-1, 1)
+
+    dir = 1;
+    ll rem = k - n * m - m - 1;
+    rep(i, k - n * m - m - 1) {
+        y++;
+        DISP;
+        x+=dir*2;
+        DISP;
+        dir *= -1;
+    }
+    y++;
+    DISP;
+    x += dir * 1;
+    DISP;
+
+
+    rep(i, traj.size()-1) {
+        assert(traj[i].fi == traj[i+1].fi || traj[i].se == traj[i+1].se );
+    }
+
+    cout << traj.size() << endl;
+    for (auto hoge : traj) {
+        cout << hoge.fi << " " << hoge.se << endl;
+    }
+
+
+//    cout << rem << endl;
+//    cout << calc(n, m) << endl;
+    return 0;
+}
