0ed2ec5094a4b4e5bdfb84ff9bbdd47baca78721
Ryo Wakatabe <wakataberyo@gmail.com>
2018-02-05 22:30:19 +0900

    added g

---
+++ atcoder/codefestival2017thanks/g.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+#include <sys/time.h>
+using namespace std;
+
+#define rep(i,n) for(long long i = 0; i < (long long)(n); i++)
+#define repi(i,a,b) for(long long i = (long long)(a); i < (long long)(b); i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define mt make_tuple
+#define mp make_pair
+template<class T1, class T2> bool chmin(T1 &a, T2 b) { return b < a && (a = b, true); }
+template<class T1, class T2> bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+
+using ll = long long; using vll = vector<ll>; using vvll = vector<vll>; using P = pair<ll, ll>;
+using ld = long double;  using vld = vector<ld>; 
+using vi = vector<int>; using vvi = vector<vi>; vll conv(vi& v) { vll r(v.size()); rep(i, v.size()) r[i] = v[i]; return r; }
+
+inline void input(int &v){ v=0;char c=0;int p=1; while(c<'0' || c>'9'){if(c=='-')p=-1;c=getchar();} while(c>='0' && c<='9'){v=(v<<3)+(v<<1)+c-'0';c=getchar();} v*=p; } // これを使うならば、tieとかを消して！！
+template <typename T, typename U> ostream &operator<<(ostream &o, const pair<T, U> &v) {  o << "(" << v.first << ", " << v.second << ")"; return o; }
+template<size_t...> struct seq{}; template<size_t N, size_t... Is> struct gen_seq : gen_seq<N-1, N-1, Is...>{}; template<size_t... Is> struct gen_seq<0, Is...> : seq<Is...>{};
+template<class Ch, class Tr, class Tuple, size_t... Is>
+void print_tuple(basic_ostream<Ch,Tr>& os, Tuple const& t, seq<Is...>){ using s = int[]; (void)s{0, (void(os << (Is == 0? "" : ", ") << get<Is>(t)), 0)...}; }
+template<class Ch, class Tr, class... Args> 
+auto operator<<(basic_ostream<Ch, Tr>& os, tuple<Args...> const& t) -> basic_ostream<Ch, Tr>& { os << "("; print_tuple(os, t, gen_seq<sizeof...(Args)>()); return os << ")"; }
+ostream &operator<<(ostream &o, const vvll &v) { rep(i, v.size()) { rep(j, v[i].size()) o << v[i][j] << " "; o << endl; } return o; }
+template <typename T> ostream &operator<<(ostream &o, const vector<T> &v) { o << '['; rep(i, v.size()) o << v[i] << (i != v.size()-1 ? ", " : ""); o << "]";  return o; }
+template <typename T> ostream &operator<<(ostream &o, const deque<T> &v) { o << '['; rep(i, v.size()) o << v[i] << (i != v.size()-1 ? ", " : ""); o << "]";  return o; }
+template <typename T>  ostream &operator<<(ostream &o, const set<T> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T>  ostream &operator<<(ostream &o, const unordered_set<T> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T, typename U>  ostream &operator<<(ostream &o, const map<T, U> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it << (next(it) != m.end() ? ", " : ""); o << "]";  return o; }
+template <typename T, typename U, typename V>  ostream &operator<<(ostream &o, const unordered_map<T, U, V> &m) { o << '['; for (auto it = m.begin(); it != m.end(); it++) o << *it; o << "]";  return o; }
+vector<int> range(const int x, const int y) { vector<int> v(y - x + 1); iota(v.begin(), v.end(), x); return v; }
+template <typename T> istream& operator>>(istream& i, vector<T>& o) { rep(j, o.size()) i >> o[j]; return i;}
+template <typename T, typename S, typename U> ostream &operator<<(ostream &o, const priority_queue<T, S, U> &v) { auto tmp = v; while (tmp.size()) { auto x = tmp.top(); tmp.pop(); o << x << " ";} return o; }
+template <typename T> ostream &operator<<(ostream &o, const queue<T> &v) { auto tmp = v; while (tmp.size()) { auto x = tmp.front(); tmp.pop(); o << x << " ";} return o; }
+template <typename T> ostream &operator<<(ostream &o, const stack<T> &v) { auto tmp = v; while (tmp.size()) { auto x = tmp.top(); tmp.pop(); o << x << " ";} return o; }
+template <typename T> unordered_map<T, ll> counter(vector<T> vec){unordered_map<T, ll> ret; for (auto&& x : vec) ret[x]++; return ret;};
+string substr(string s, P x) {return s.substr(x.fi, x.se - x.fi); }
+void vizGraph(vvll& g, int mode = 0, string filename = "out.png") { ofstream ofs("./out.dot"); ofs << "digraph graph_name {" << endl; set<P> memo; rep(i, g.size())  rep(j, g[i].size()) { if (mode && (memo.count(P(i, g[i][j])) || memo.count(P(g[i][j], i)))) continue; memo.insert(P(i, g[i][j])); ofs << "    " << i << " -> " << g[i][j] << (mode ? " [arrowhead = none]" : "")<< endl;  } ofs << "}" << endl; ofs.close(); system(((string)"dot -T png out.dot >" + filename).c_str()); }
+size_t random_seed; namespace std { using argument_type = P; template<> struct hash<argument_type> { size_t operator()(argument_type const& x) const { size_t seed = random_seed; seed ^= hash<ll>{}(x.fi); seed ^= (hash<ll>{}(x.se) << 1); return seed; } }; }; // hash for various class
+struct timeval start; double sec() { struct timeval tv; gettimeofday(&tv, NULL); return (tv.tv_sec - start.tv_sec) + (tv.tv_usec - start.tv_usec) * 1e-6; }
+struct init_{init_(){ ios::sync_with_stdio(false); cin.tie(0); gettimeofday(&start, NULL); struct timeval myTime; struct tm *time_st; gettimeofday(&myTime, NULL); time_st = localtime(&myTime.tv_sec); srand(myTime.tv_usec); random_seed = RAND_MAX / 2 + rand() / 2; }} init__;
+#define ldout fixed << setprecision(40) 
+
+#define EPS (double)1e-14
+#define INF (ll)1e18
+#define mo  (ll)(1e9+7)
+
+
+// 使い方
+//
+// 隣接行列としてsを埋める
+// initを呼ぶ
+// calcを計算
+// valに最大独立集合の大きさが入っている
+
+int s[40][40]; // 隣接行列
+long long S[40]; // 隣接行列をbitwiseで詰め込んだもの
+int v[40]; // 頂点の利用状況
+int val; // 最大独立集合の数
+int n; // 頂点数
+long long uk=0; // v[i]=0のiを詰め込んだもの
+
+void calc(){
+    int nm=0;
+    rep(i, n) if(v[i] == 1) nm++;
+    val = max(val, nm);
+
+    rep(i, n) {
+        if (v[i] == 0 && __builtin_popcountll(uk & S[i]) < 2){
+            v[i] = 1;
+            uk -= 1ll << i;
+            long long ch=0;
+            rep(j, n) if (v[j] == 0 && s[i][j]) {
+                v[j] = 2;
+                ch += 1ll << j;
+                uk -= 1ll << j;
+            }
+            calc();
+            v[i] = 0;
+            uk += 1ll<<i;
+            rep(j, n) if (ch & (1ll << j)) v[j] = 0, uk += 1LL<<j;
+            return;
+        }
+    }
+    int dm = 1;
+    int at = -1;
+    rep(i, n) if(v[i] == 0 && __builtin_popcountll(uk & S[i]) >= dm){
+        dm = __builtin_popcountll(uk & S[i]);
+        at = i;
+    }
+    if(at == -1) return;
+    v[at] = 2;
+    uk -= 1LL<<at;
+    calc();
+    v[at] = 0;
+    uk += 1LL<<at;
+
+    v[at] = 1;
+    uk -= 1LL<<at;
+    long long ch=0;
+    rep(j, n) if(v[j] == 0 && s[at][j]) {
+        v[j] = 2;
+        uk -= 1ll << j;
+        ch += 1ll << j ;
+    }
+    calc();
+    rep(j, n) if(ch & 1ll << j) { v[j] = 0; uk += 1ll << j; }
+    v[at] = 0;
+    uk += 1ll << at;
+    return;
+}
+void init(){
+    rep(j, n) v[j] = 0;
+    rep(j, n) {
+        S[j] = 0;
+        rep(k, n) if (s[j][k]) S[j] |= (1ll << k);
+    }
+    val = 0;
+    uk = (1ll<<n) - 1;
+}
+
+int main(void) {
+    ll m; cin >> n >> m;
+    rep(i, m) {
+        ll u, v; cin >> u >> v; u--, v--;
+        s[u][v] = 1;
+        s[v][u] = 1;
+    }
+    init();
+    calc();
+    cout << val << endl;
+
+    return 0;
+}
